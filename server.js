28b616348911349e6c4cf4cd64705aa3737797b4
Adrian Bautista <adrianbautista@users.noreply.github.com>
2019-03-13 17:49:13 -0400

    Add unique ID to each connection

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
+const uuid = require('uuid');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
@@ -13,32 +14,34 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
-var store = { objs: [] }
+var store = { users: [] }
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.id = uuid.v4();
+
   ws.on('close', () => console.log('Client disconnected'));
 
   ws.on('message', (data) => {
-    console.log("message received: " + data)
-    store.objs.push(data);
+    store.users.push({ 'id': ws.id, deviceData: data });
   });
 });
 
 setInterval(() => {
   wss.clients.forEach((client) => {
-    var sample = store.objs[0];
+    var sample = store.users[0];
     // console.log("sending to clients: " + JSON.stringify({ 'foo': sample }));
-    console.log("hello world");
+    // console.log("clients connected", wss.clients.map( c => c.id));
+    console.log("store: ", store);
 
     // console.log("sending to clients: ", store);
     // client.send(JSON.stringify(store));
 
     if (sample) {
       // client.send(JSON.stringify({ 'foo': sample }));
-      client.send("hello world");
+      client.send(JSON.stringify(sample));
     }
 
-    store.objs = [];
+    store.users = [];
   });
 }, 1000);
